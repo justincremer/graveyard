9fd39f6b0c75a5e9a6ff8bf46caf5c168f1078c3
jacremer <jacremer@live.com>
2021-03-31 12:34:07 -0700

    fiber start

---
+++ Go/Fiber/go.mod
@@ -0,0 +1,5 @@
+module src/main.go
+
+go 1.16
+
+require github.com/gofiber/fiber/v2 v2.7.1 // indirect
---
+++ Go/Fiber/src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	}).Get("/users", func(c *fiber.Ctx) error {
+		return c.SendString("xiuxiu")
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
